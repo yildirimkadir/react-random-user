b6c99fdec8f6310f20303ee8adfca20225ed7bdb
yildirimkadir <yildirimkad34@gmail.com>
2022-07-18 16:41:36 +0200

    "Das Projekt wurde erstellt."

--- src/App.js
+++ src/App.js
@@ -12,109 +12,75 @@ import Footer from "./components/footer/Footer";
 import { useState,useEffect } from "react";
 import axios from 'axios';
 
+const url = "https://randomuser.me/api/";
 
 
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+// const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
-  const [users, setUsers] = useState();
-  const [visible, setVisible] = useState(false);
-  const [userInfo, setUserInfo] = useState({
-    user: "name",
-    mail: "",
-    age: "",
-    map: "",
-    phone: "1",
-    lock: "" 
-  });
+  const [users, setUsers] = useState([]);
+  const [userList, setUserList] = useState([]);
+  const [title, setTitle ] = useState("name");
+  const [value, setValue ] = useState("");
+  const [picture, setPicture ] = useState("");
+  const [loading, setLoading ] = useState(true);
 
-  const [title, setTitle] = useState({
-    user: "",
-    mail: "",
-    age: "",
-    map: "",
-    phone: "",
-    lock: "" 
 
-  });
-
-  const url = "https://randomuser.me/api/";
+  
+ 
   const getUsers = async () => {
       try {
-        const response = await axios.get(url).then((response)=>setUsers(response.data.results[0]))
-          // console.log(users);
+         setLoading(true); // buna gerek yok 
+        const { data } = await axios.get(url);
+        setUsers(data.results[0]);
+        setValue(Object.values(data.results[0].name).splice(1).join(" "));
+        setPicture(data.results[0].picture.large);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     };
-  
+
+    // console.log(users.data.results[0].name.first);
+
+
     useEffect(() => {
       getUsers();
+      
     }, []);
 
-    const handleShow = (e)=> {
-     setVisible(true);
-     if (!visible) {
+    useEffect(() => {
+      setTitle("name");
       
-        if (e.target.alt ==="user") {
-            setUserInfo({...userInfo, [e.target.alt]: `${users.name.first + " " + users.name.last}` });
-            setTitle({...title, [e.target.alt]: "name"})
-            alert("göründüm")}
-            console.log(userInfo.user);
-            console.log(users.name.first);
-            console.log(title);
-       if (e.target.alt ==="mail") {
-        //  setUserInfo({...userInfo, name: users.name.first + " " + users.name.last})}
-            alert("ben mailim")}
-       if (e.target.alt ==="age") {
-        //  setUserInfo({...userInfo, name: users.name.first + " " + users.name.last})}
-            setTitle({...title, [e.target.alt]: e.target.alt})
-            alert("age göründüm")}
-            console.log(title);
-       if (e.target.alt ==="map") {
-        //  setUserInfo({...userInfo, name: users.name.first + " " + users.name.last})}
-            alert("street göründüm")}
-       if (e.target.alt ==="phone") {
-        //  setUserInfo({...userInfo, name: users.name.first + " " + users.name.last})}
-            alert("phone göründüm")}
-       if (e.target.alt ==="lock") {
-        //  setUserInfo({...userInfo, name: users.name.first + " " + users.name.last})}
-            alert("password göründüm")}
-      } 
+    }, [users]);
+
+    const handleNewUser = () => {
+      // setLoading(true)
+      getUsers();
     }
-    const handleTitle = (e) => {
-      if (e.target.alt==="user")
-      setTitle()
       
+    const handleAddUser= () => {
+      const added = {
+        id: users.login.uuid,
+        firstname: users.name.first,
+        email: users.email,
+        phone: users.phone,
+        age: users.dob.age
+      }
+      if(userList.filter((item) => item.id==users.login.uuid).length > 0) {
+        alert("Dies User wurde schon hinzugefügt.")
+      } else {
+        setUserList((oldList)=> [...oldList, added]);
+      }
     }
 
-    //   if (e.target.getAttribute("data-label") === "name") {
-    //     setUserInfo({...userInfo, name: users.name.first + " " + users.name.last})}
-
-        
-
-    //   if (e.target.getAttribute("data-label") ==="email") {
-    //     setUserInfo({...userInfo, email: users.email})} 
-
-
-    //   if (e.target.getAttribute("data-label") ==="age") {
-    //     setUserInfo({...userInfo, age:users.dob.age })} 
-
       
-    //   setUserInfo({...userInfo, email: users.email})
-    //   setUserInfo({...userInfo, age: users.dob.age})
-    //   setUserInfo({...userInfo, street: users.location.street.number + " " + users.location.street.name})
-    //   setUserInfo({...userInfo, phone: users.phone})
-    //   setUserInfo({...userInfo, password: users.login.password})
-
       
-    //  }
-    // }
+     
 
-    const handleClose = () => {
-      setVisible(false);
-    }
 
+
+  
 
 
 
@@ -124,35 +90,34 @@ function App() {
         <img src={cwSvg} alt="cw" id="cw" />
       </div>
       <div className="block">
-        <div className="container">
-          <img src={defaultImage} alt="random user" className="user-img" />
-          <p className="user-title" id="title">My {handleTitle} is</p>
-          <p className="user-value" id="value"></p>
+        <div className="container" >
+          <img src={picture&&picture} alt="random user" className="user-img" />
+          <p className="user-title" id="title">My {title} is </p>
+          <p className="user-value" id="value">{value}</p>
           <div className="values-list">
-            <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" onMouseEnter={handleShow} onMouseLeave={handleClose} />
+            <button className="icon" data-label="name" onMouseEnter={() => {setValue(Object.values(users.name).splice(1).join(" ")); setTitle("name")}} >
+              <img src={users.gender==="female"? womanSvg : manSvg} alt="user" id="iconImg" />
             </button>
-            <button className="icon" data-label="email" >
-              <img src={mailSvg} alt="mail" id="iconImg" onMouseEnter={handleShow} onMouseLeave={handleClose}/>
+            <button className="icon" data-label="email" onMouseEnter={() => {setValue((users.email)); setTitle("email")}}>
+              <img src={mailSvg} alt="mail" id="iconImg" /> 
+            </button>  
+              <button className="icon" data-label="age" onMouseEnter={() => {setValue((users.dob.age)); setTitle("age")}}>
+              <img src={womanAgeSvg} alt="age" id="iconImg" />
             </button>
-            <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" onMouseEnter={handleShow} onMouseLeave={handleClose} />
+            <button className="icon" data-label="street" onMouseEnter={()=> {setValue((users.location.street.name)); setTitle("adresse")}}>
+              <img src={mapSvg} alt="map" id="iconImg" />
             </button>
-            <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" onMouseEnter={handleShow} onMouseLeave={handleClose} />
+            <button className="icon" data-label="phone" onMouseEnter={() => {setValue((users.phone)); setTitle("phone")}}>
+              <img src={phoneSvg} alt="phone" id="iconImg" />
             </button>
-            <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" onMouseEnter={handleShow} onMouseLeave={handleClose} />
-            </button>
-            <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" onMouseEnter={handleShow} onMouseLeave={handleClose} />
+            <button className="icon" data-label="password"onMouseEnter={() => { setValue((users.login.password)); setTitle("password")}}>
+              <img src={padlockSvg} alt="lock" id="iconImg" />
             </button>
           </div>
           <div className="btn-group">
-            <button className="btn" type="button">
-              new user
+            <button className="btn" type="button" onClick={handleNewUser}>{loading === true ? "Loading" : "New User"}
             </button>
-            <button className="btn" type="button">
+            <button className="btn" type="button" onClick={handleAddUser}>
               add user
             </button>
           </div>
@@ -167,7 +132,20 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <tr className="body-tr"></tr>
+                {userList?.map((item) => {
+                  return (
+                    <tr className="body-tr">
+                    <td>{item.firstname}</td>
+                    <td>{item.email} </td>
+                    <td>{item.phone} </td>
+                    <td>{item.age} </td>
+                    </tr>
+                    
+                    )
+                })}
+
+
+
             </tbody>
           </table>
         </div>
